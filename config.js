8e75e81fbb0161c58fe779660d44397ba0eef1dc
Govarthini24 <163633922+Govarthini24@users.noreply.github.com>
2024-03-16 16:54:19 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+export const API_URL = "https://jcjyrsoctjtndmgmjqau.supabase.co";
+export const EMAIL_KEY = "6oJ37brWr97VFjdJs";
+export const KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impjanlyc29jdGp0bmRtZ21qcWF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1ODAzMzMsImV4cCI6MjAyNjE1NjMzM30.07rW-Dj016cOSaeuh7EAOI78Ucg9h1qNWV-UmqJaBeg";
---
+++ script.js
@@ -0,0 +1,453 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { API_URL, EMAIL_KEY, KEY } from "./config.js";
+
+import { emailjs } from "./email.min.js";
+
+
+
+
+// Initialize Supabase client
+const supabase = createClient(API_URL, KEY);
+
+function validateEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function validateMobileNo(mobileNo) {
+  return /^[0-9]{10}$/.test(mobileNo);
+}
+
+function validateAdhaarNo(adhaarNo) {
+  return /^[0-9]{12}$/.test(adhaarNo);
+}
+
+const form_registration_submit = document.getElementById(
+  "form-registration-submit"
+);
+
+form_registration_submit?.addEventListener("submit", async (event) => {
+  event.preventDefault(); // Prevent default form submission
+
+  // Collect form data
+  const email = document.getElementById("signup-email").value;
+  const mobileNo = document.getElementById("mobile-no").value;
+  const adhaarNo = document.getElementById("adhaar-no").value;
+
+  // Validate form data
+  if (!validateEmail(email)) {
+    alert("Please enter a valid email address");
+    return;
+  }
+  if (!validateMobileNo(mobileNo)) {
+    alert("Please enter a valid mobile number");
+    return;
+  }
+  if (!validateAdhaarNo(adhaarNo)) {
+    alert("Please enter a valid Aadhaar number");
+    return;
+  }
+  
+  // Collect form data
+  const voterID = document.getElementById("voter-ID").value;
+  const userName = document.getElementById("user-name").value;
+  const userAge = document.getElementById("user-age").value;
+  const userGender = document.getElementById("gender").value;
+  const address = document.getElementById("address").value;
+  const district = document.getElementById("district").value;
+  const state = document.getElementById("state").value;
+  const password = document.getElementById("signup-password").value;
+  const confirmPassword = document.getElementById(
+    "signup-password-confirm"
+  ).value;
+
+  // Check if passwords match
+  if (password !== confirmPassword) {
+    alert("Passwords do not match");
+    return;
+  }
+
+  if(!(userAge>=18)){
+    alert("Your Age is lesser than 18, Register once your age is equal to or greater than 18 years");
+    return;
+  }
+
+  // Validate form data if necessary
+
+  try {
+    const { data, error } = await supabase
+      .from("UserData")
+      .insert([
+        {
+          email_id: email,
+          mobile_no: mobileNo,
+          adhaar_no: adhaarNo,
+          voter_id: voterID,
+          user_name: userName,
+          user_age: userAge,
+          user_gender: userGender,
+          address: address,
+          district: district,
+          state: state,
+          password: password,
+        },
+      ])
+      .select();
+
+    if (error) {
+      if (error.message.toString().startsWith("duplicate")) {
+        alert("Already Registered");
+        console.error(error);
+        window.location.href = "index.html";
+        return;
+      } else {
+        alert("Error fetching user data from Supabase");
+        console.error(error);
+        return;
+      }
+    } else {
+      alert("User data updated successfully");
+      // Redirect to success page or perform any other action
+      window.location.href = "index.html";
+    }
+  } catch (error) {
+    alert("Try Again");
+    console.error(error);
+  }
+});
+
+const loginForm = document.getElementById("login-form");
+
+loginForm?.addEventListener("submit", async (event) => {
+  event.preventDefault(); // Prevent default form submission
+
+  // Collect form data
+  const email = document.getElementById("login-email").value;
+  const password = document.getElementById("login-password").value;
+
+  try {
+    // Check if the email exists in the database
+    const { data: userData, error: userError } = await supabase
+      .from("UserData")
+      .select("*")
+      .eq("email_id", email)
+      .single();
+
+    if (!userData) {
+      // If no user found with this email
+      alert("Invalid User, Please register by clicking New User");
+      return;
+    }
+
+    // Compare the password with the one stored in the database
+    if (userData.password !== password) {
+      // If password doesn't match
+      alert("Invalid  password");
+      return;
+    }
+
+    const userDistrict = userData.district;
+    const userState = userData.state;
+    const userID = userData.id;
+
+    sessionStorage.userDistrict = userDistrict;
+    sessionStorage.userState = userState;
+    sessionStorage.userID = userID;
+
+    // Authentication successful
+    alert("Login successful!");
+    // Redirect to voter page or perform any other action
+    window.location.href = "voterpage.html";
+  } catch (error) {
+    alert("An error occurred while processing the login");
+    console.error(error);
+  }
+});
+
+// Function to fetch and display candidate data
+async function displayCandidates(userDistrict, userState) {
+  try {
+    const { data: candidates, error } = await supabase
+      .from("ElectrolCandidates")
+      .select("*")
+      .order("id");
+
+    if (error) {
+      throw error;
+    }
+
+    if (!candidates || candidates.length === 0) {
+      // Handle case where no candidates are found
+      console.log("No candidates found");
+      return;
+    }
+
+    const candidatesContainer = document.getElementById("candidates-container");
+
+    // Clear previous content if any
+    candidatesContainer.innerHTML = "";
+
+    // Filter candidates based on user's district and state
+    const filteredCandidates = candidates.filter((candidate) => {
+      return (
+        candidate.district.toLowerCase() === userDistrict &&
+        candidate.state.toLowerCase() === userState
+      );
+    });
+
+    // Iterate through candidates and display information
+    filteredCandidates.forEach((candidate, index) => {
+      const candidateDiv = document.createElement("div");
+      candidateDiv.classList.add("voters-card");
+
+      // Display candidate information
+      candidateDiv.innerHTML = `<div class="voters-info">   
+      <div class="voters-svg">
+            <div>
+              <img src="${candidate.party_image}" alt="${candidate.party_name}" class="avatar" />
+            </div>
+            <div>
+              <img src="${candidate.avatar}" alt="${candidate.candidate_name}" class="avatar" />
+            </div>
+          </div>
+        </div>
+          <div class="voters-information">
+            <div class="voter-details">
+              <p>Name: <span class="non-bold">${candidate.candidate_name}</span></p>
+              <p>Party: <span class="non-bold">${candidate.party_name}</span></p>
+              <p>District: <span class="non-bold">${candidate.district}</span></p>
+              <p>State: <span class="non-bold">${candidate.state}</span></p>          
+            </div>      
+            <div class="votes-value-radio">
+              <input type="radio" name="candidate" value="${candidate.id}" id="candidate-${index}">
+            </div>
+          </div>
+        </div>
+      </div>`;
+
+      candidatesContainer.appendChild(candidateDiv);
+    });
+  } catch (error) {
+    console.error("Error fetching candidates:", error);
+  }
+}
+
+// Function to handle vote submission
+async function submitVote(userID) {
+  const selectedCandidateId = document.querySelector(
+    'input[name="candidate"]:checked'
+  );
+
+  if (!selectedCandidateId) {
+    alert("Please select a candidate to vote");
+    return;
+  }
+
+  const candidateId = selectedCandidateId.value;
+
+  // Process vote submission, you can implement this logic based on your requirements
+  console.log("Voting for candidate with ID:", candidateId);
+  console.log("User ID:", userID);
+
+  try {
+    // Fetch user data from UserData table
+    const { data: userData, error: userError } = await supabase
+      .from("UserData")
+      .select("*")
+      .eq("id", userID) // Assuming userId is the ID of the user
+      .single();
+
+    if (userError) {
+      throw userError;
+    }
+
+    if (!userData) {
+      alert("User not found");
+      return;
+    }
+
+    // Fetch candidate data from ElectrolCandidates table
+    const { data: candidateData, error: candidateError } = await supabase
+      .from("ElectrolCandidates")
+      .select("*")
+      .eq("id", candidateId)
+      .single(); // Assuming candidateId is the ID of the candidate
+
+    if (candidateError) {
+      throw candidateError;
+    }
+
+    if (!candidateData || candidateData.length === 0) {
+      alert("Candidate not found");
+      return;
+    }
+
+    // Save vote data to Votes table
+    const { error: voteError } = await supabase.from("Votes").insert([
+      {
+        user_id: userData.id,
+        user_email_id: userData.email_id,
+        user_mobile_no: userData.mobile_no,
+        user_adhaar_no: userData.adhaar_no,
+        user_name: userData.user_name,
+        user_age: userData.user_age,
+        user_gender: userData.user_gender,
+        user_voter_id: userData.voter_id,
+        user_address: userData.address,
+        user_district: userData.district,
+        user_state: userData.state,
+
+        candidate_id: candidateData.id,
+        candidate_party_image: candidateData.party_image,
+        candidate_avatar: candidateData.avatar,
+        candidate_name: candidateData.candidate_name,
+        candidate_party_name: candidateData.party_name,
+        candidate_district: candidateData.district,
+        candidate_state: candidateData.state,
+
+        // Add other necessary fields here
+      },
+    ]);
+
+    if (voteError) {      
+      throw voteError;
+    }
+    
+
+    // If everything is successful
+    alert("Vote saved successfully");
+    emailjs.init(EMAIL_KEY); 
+    emailjs.send("service_f5sksdi","template_6l94nas",{
+      to_name: userData.user_name,
+      from_name: "Election Commission",
+      message: "Your vote successfully submitted to " +candidateData.candidate_name+" and the Party Name is "+candidateData.party_name+".",
+      to_email: userData.email_id,
+      });
+      if(userID==="1"){        
+        window.location.href = "dashboardpage.html";}else{
+        window.location.href = "index.html";
+     }
+  } catch (error) {
+    if(error.message.toString().startsWith("duplicate")){
+      alert("Already Submitted your vote");
+    }else{
+      alert("An error occurred. Please try again.");
+      
+    }
+  }
+}
+
+const candidatesContainer = document.getElementById("candidates-container");
+let userDistrict = sessionStorage.userDistrict;
+let userState = sessionStorage.userState;
+let userID = sessionStorage.userID;
+
+if (candidatesContainer) {
+  displayCandidates(userDistrict, userState);  
+  if(userID==="1"){
+    const dashboardContainer = document.getElementById("dashboard");
+    dashboardContainer.setAttribute("style","display:inline-block");
+  }
+}
+
+const voteSubmitValue = document.getElementById("vote-submit");
+
+voteSubmitValue?.addEventListener("submit", async (event) => {
+  event.preventDefault(); // Prevent default form submission
+  submitVote(userID);
+});
+
+const logOutElement = document.getElementById("log-out-button-id");
+
+logOutElement?.addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+
+const dashboardElement = document.getElementById("dashboard");
+dashboardElement?.addEventListener("click", function () {
+  window.location.href = "dashboardpage.html";
+});
+
+
+async function displayAllCandidates() {
+  try {
+    const { data: candidates, error } = await supabase
+      .from("ElectrolCandidates")
+      .select("*")
+      .order("id");
+
+    if (error) {
+      throw error;
+    }
+
+    if (!candidates || candidates.length === 0) {
+      // Handle case where no candidates are found
+      console.log("No candidates found");
+      return;
+    }
+
+    const candidatesContainer = document.getElementById("candidates-container-dashboard");
+
+    // Clear previous content if any
+    candidatesContainer.innerHTML = "";
+
+    // Iterate through candidates and display information
+    for (let index = 0; index < candidates.length; index++) {
+      const candidate = candidates[index];
+
+      // Fetch vote count for the current candidate
+      const { data: votes, error: voteError } = await supabase
+        .from("Votes")
+        .select("count", { count: "exact" })
+        .eq("candidate_id", candidate.id);
+
+      if (voteError) {
+        throw voteError;
+      }
+
+      const voteCount = votes ? votes[0].count : 0;
+
+      const candidateDiv = document.createElement("div");
+      candidateDiv.classList.add("voters-card");
+
+      // Display candidate information and vote count
+      candidateDiv.innerHTML = `<div class="voters-info">
+          <div class="voters-svg">
+            <div>
+              <img src="${candidate.party_image}" alt="${candidate.party_name}" class="avatar" />
+            </div>
+            <div>
+              <img src="${candidate.avatar}" alt="${candidate.candidate_name}" class="avatar" />
+            </div>
+          </div>
+        </div>
+          <div class="voters-information">
+            <div class="voter-details">
+              <p>Name: <span class="non-bold">${candidate.candidate_name}</span></p>
+              <p>Party: <span class="non-bold">${candidate.party_name}</span></p>
+              <p>District: <span class="non-bold">${candidate.district}</span></p>
+              <p>State: <span class="non-bold">${candidate.state}</span></p>          
+            </div>      
+            <div class="votes-value">
+              <div>
+                <p class="non-bold votescount">${voteCount}</p>
+              </div>
+              <div>
+                <p class="votescount-name">Vote Count</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+
+      candidatesContainer.appendChild(candidateDiv);
+    }
+  } catch (error) {
+    console.error("Error fetching candidates:", error);
+  }
+}
+
+const candidatesContainerDashboard= document.getElementById("candidates-container-dashboard");
+
+if (candidatesContainerDashboard) {
+  displayAllCandidates();
+}
